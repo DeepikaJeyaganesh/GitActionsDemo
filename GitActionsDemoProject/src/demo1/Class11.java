16d94e596e749f708608d0ee81d96cba86beec5f
DeepikaJeyaganesh <kdeepika101@gmail.com>
2023-10-25 13:07:11 +0530

    add 2 numbers

--- GitActionsDemoProject/src/demo1/Class11.java
+++ GitActionsDemoProject/src/demo1/Class11.java
@@ -1,10 +1,16 @@
 package demo1;
 
+import java.util.Scanner;
+
 public class Class11 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int result=a+b;
+		System.out.println("Answer is "+result);
 	}
 
 }
